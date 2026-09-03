f794e8cff1aaa0ce0fbda185f7310b5d5cf8f4f6
MNihal7961 <mnihal7961@gmail.com>
2026-09-03 18:40:36 +0530

    feat: add react-toastify for notification handling in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const App:React.FC= () => {
   return (
-    <div>App</div>
+    <div>
+      App
+      <ToastContainer position="top-right" autoClose={3000} />
+    </div>
   )
 }
 
