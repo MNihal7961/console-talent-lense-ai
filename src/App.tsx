bcf46b456a2e0bd7ba93a484203b9b6f7114373e
MNihal7961 <mnihal7961@gmail.com>
2026-09-04 12:38:25 +0530

    feat: enhance App component with ConfigProvider and toast styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { ConfigProvider } from "antd";
+import React from "react";
+import { ToastContainer } from "react-toastify";
+import Router from "./router/Router";
+import "react-toastify/dist/ReactToastify.css";
 
-const App:React.FC= () => {
+const App: React.FC = () => {
   return (
-    <div>
-      App
-      <ToastContainer position="top-right" autoClose={3000} />
-    </div>
-  )
-}
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#4f46e5",
+          colorLink: "#4f46e5",
+          colorError: "#e5484d",
+          colorSuccess: "#17a768",
+          colorWarning: "#e8a531",
+        },
+      }}
+    >
+      <Router />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar
+        closeOnClick
+        pauseOnHover
+        newestOnTop
+        className="app-toast-container"
+        toastClassName="app-toast"
+      />
+    </ConfigProvider>
+  );
+};
 
-export default App
+export default App;
