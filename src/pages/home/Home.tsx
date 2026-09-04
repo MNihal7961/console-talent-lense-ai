d9b8b511f631632d17422c859f839353632fe0ca
MNihal7961 <mnihal7961@gmail.com>
2026-09-04 12:59:32 +0530

    feat: enhance Home component to display user greeting and logout functionality

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Button, Typography } from "antd";
+import { useAuth } from "../../contexts/AuthContext";
 
-const Home = () => {
+const Home: React.FC = () => {
+  const { user, logout } = useAuth();
+
   return (
-    <div>Home</div>
-  )
-}
+    <div style={{ padding: 24 }}>
+      <Typography.Title level={4}>
+        Welcome{user ? `, ${user.firstName} ${user.lastName}` : ""}
+      </Typography.Title>
+      <Button onClick={logout}>Sign out</Button>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
