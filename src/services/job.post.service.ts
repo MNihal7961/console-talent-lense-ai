1f3de4b878cb3a5bdd6d7bd19db2ed8560eba834
MNihal7961 <mnihal7961@gmail.com>
2026-09-06 19:51:22 +0530

    feat: update JobPostService to include type annotations for methods

--- src/services/job.post.service.ts
+++ src/services/job.post.service.ts
@@ -1,9 +1,15 @@
 import apiClient from "./api.client.service";
+import type { JobPost } from "../interface/job-post";
 
 export class JobPostService {
-  async getJobPosts() {
+  async getJobPosts(): Promise<JobPost[]> {
     const response = await apiClient.get("/job-post");
     return response.data || [];
+  }
+
+  async findJobPostById(jobPostId: string): Promise<JobPost | null> {
+    const response = await apiClient.get(`/job-post/${jobPostId}`);
+    return response.data || null;
   }
 }
 
